f0ed5cc87a966cc2f78606f44259656342341b26
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-09-03 12:20:05 -0300

    Index executando Questão 3

--- index.php
+++ index.php
@@ -6,11 +6,18 @@ use App\Questao\Questoes;
 
 // Executando primeira questão
 echo 'Primeira Questão: </br>';
-echo Questoes::Questao1(5, 10);
+echo 'O maior número é '. Questoes::Questao1(5, 10);
 
 echo '</br>';
 echo '</br>';
 
 // Executando segunda questão
 echo 'Segunda Questão: </br>';
-echo Questoes::Questao2(10, 2);
+echo 'O menor número é ' . Questoes::Questao2(10, 2);
+
+echo '</br>';
+echo '</br>';
+
+// Executando terceira questão
+echo 'Terceira Questão: </br>';
+echo 'O maior número é '. Questoes::Questao3(10, 50, 5);
