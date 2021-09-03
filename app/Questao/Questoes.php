3ad3b1afd6a415c9509ded3b7024b19629619633
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2021-09-03 12:09:39 -0300

    Criada Segunda Questao na Classe Questoes

--- app/Questao/Questoes.php
+++ app/Questao/Questoes.php
@@ -10,10 +10,21 @@ class Questoes {
     */
     public static function Questao1($val1, $val2) {
         if($val1 > $val2) {
-            return $val1;
+            return 'O maior número é '. $val1;
         }
 
-        return $val2;
+        return 'O maior número é ' . $val2;
     }
 
+    /**
+    * Crie uma função em linguagem PHP que receba 2 números e retorne
+    * o menor valor entre eles.
+    */
+    public static function Questao2($val1, $val2) {
+        if($val1 < $val2) {
+            return 'O menor número é '. $val1;
+        }
+
+        return 'O menor número é '. $val2;
+    }
 }
